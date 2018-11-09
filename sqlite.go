2959c28dc2136e5a0fe843fa33e008d27ba36204
Shirley Leu <shirley.leu@schibsted.com>
2018-11-09 19:32:14 +0100

    Implement searchTranscripts on comicManagerSQLite

--- sqlite.go
+++ sqlite.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type dbComic struct{
 	date int
-	transcript string
 }
 
 type comicManagerSQLite struct {
-	c *sql.DB
+	db *sql.DB
 }
 
 func newComicManagerSQLite(source string) (*comicManagerSQLite, error){
@@ -22,7 +22,21 @@ func newComicManagerSQLite(source string) (*comicManagerSQLite, error){
 	return &comicManagerSQLite{db}, nil
 }
 
-func (m comicManagerSQLite) searchTranscripts(searchTerm string) ([]dbComic, error){
-
-	return []dbComic{}, nil
+func (m *comicManagerSQLite) searchTranscripts(searchTerm string) ([]dbComic, error){
+	query := `SELECT date FROM search_transcripts WHERE transcript MATCH $1`
+	rows, err := m.db.QueryContext(context.TODO(), query, searchTerm)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var found []dbComic
+	for rows.Next(){
+		var date int
+		err = rows.Scan(&date)
+		if err != nil {
+			return nil, err
+		}
+		found = append(found, dbComic{date})
+	}
+	return found, nil
 }
